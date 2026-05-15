2d7b7c58b0a2fd57429f63209f2187b7dcf39ec3
lv-fool <2097531961@qq.com>
2026-05-15 22:22:26 +0800

    Revert "Merge branch 'main' of https://github.com/ZJD521/xxl_v0.2"
    This reverts commit 35a791fff70b809c06d7dccdb3e8d3b77f305cc4, reversing
    changes made to 9c5b16334cfbdc9451c7b38533efb86cf5e99fa5.

--- game_btns_cb.c
+++ game_btns_cb.c
@@ -9,21 +9,12 @@ extern lv_obj_t * btn_exit;
 extern bac bg;
 extern lv_obj_t * scr_menu; 
 extern lv_obj_t * scr_game;
-extern lv_timer_t * gametime;
-extern char buf[60];
 void btn_start_cb(lv_event_t*e){  //直接切屏进入游戏
 	  lv_event_code_t code = lv_event_get_code(e); 
 	  if(code == LV_EVENT_CLICKED){
 	  srand(lv_tick_get());
 
 	  lv_scr_load(scr_game);
-		lv_obj_add_flag(btn_start,LV_OBJ_FLAG_HIDDEN);
-		lv_obj_add_flag(btn_choose,LV_OBJ_FLAG_HIDDEN);
-			
-		lv_obj_set_size(btn_exit,128,50);  
-		lv_obj_align(btn_exit,LV_ALIGN_CENTER,430,150); 
- 		lv_obj_set_parent(btn_exit,scr_game);
-
 		}
 		
 	 
@@ -35,12 +26,11 @@ void btn_choose_cb(lv_event_t*e){  //加载两个主题按钮
 	  srand(lv_tick_get());
 		lv_obj_add_flag(btn_start,LV_OBJ_FLAG_HIDDEN);
 		lv_obj_add_flag(btn_choose,LV_OBJ_FLAG_HIDDEN);
-			
-    lv_obj_clear_flag(btn_def,LV_OBJ_FLAG_HIDDEN);
-		lv_obj_clear_flag(btn_ice,LV_OBJ_FLAG_HIDDEN);
-		lv_obj_clear_flag(btn_exit,LV_OBJ_FLAG_HIDDEN);
-    
-	  
+    game_btn_theme_default_load();
+	  game_btn_theme_ice_load();
+		game_btn_exit_load();
+	  lv_obj_del(btn_choose);
+    lv_obj_del(btn_start);
 		}
 
 }
@@ -48,13 +38,7 @@ void btn_def_cb(lv_event_t*e){   //标记主题并切屏进入游戏
 	  lv_event_code_t code = lv_event_get_code(e); 
 	  if(code == LV_EVENT_CLICKED){
     srand(lv_tick_get());
-		lv_obj_add_flag(btn_def,LV_OBJ_FLAG_HIDDEN);
-		lv_obj_add_flag(btn_ice,LV_OBJ_FLAG_HIDDEN);
-	
-		lv_obj_set_size(btn_exit,128,50);  
-		lv_obj_align(btn_exit,LV_ALIGN_CENTER,430,150); 
- 		lv_obj_set_parent(btn_exit,scr_game);
-			
+
 	  bg.theme=DEFAULT;
     lv_scr_load(scr_game);
 		}
@@ -65,47 +49,26 @@ void btn_ice_cb(lv_event_t*e){   ////标记主题并切屏进入游戏
 	  lv_event_code_t code = lv_event_get_code(e); 
 	  if(code == LV_EVENT_CLICKED){
 	  srand(lv_tick_get());
-		lv_obj_add_flag(btn_def,LV_OBJ_FLAG_HIDDEN);
-		lv_obj_add_flag(btn_ice,LV_OBJ_FLAG_HIDDEN);
-	
-		lv_obj_set_size(btn_exit,128,50);  
-		lv_obj_align(btn_exit,LV_ALIGN_CENTER,430,150); 
- 		lv_obj_set_parent(btn_exit,scr_game);
-			
+
   	bg.theme=ICE;
     lv_scr_load(scr_game);
     }
 	 
 }
 void btn_exit_cb(lv_event_t*e){    //返回
-    lv_event_code_t code = lv_event_get_code(e); 
-    if(code == LV_EVENT_CLICKED && lv_scr_act() == scr_menu){
-        lv_obj_add_flag(btn_ice, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(btn_def, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(btn_exit, LV_OBJ_FLAG_HIDDEN);
-        
-        lv_obj_clear_flag(btn_start, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_clear_flag(btn_choose, LV_OBJ_FLAG_HIDDEN);
-    }
-    else if(code == LV_EVENT_CLICKED && lv_scr_act() == scr_game){
-        // 1. 停止游戏
-        game_over = 1;
-        
-        // 2. 清理定时器
-        if (gametime != NULL) {
-            lv_timer_del(gametime);
-            gametime = NULL;
-        }
+	  lv_event_code_t code = lv_event_get_code(e); 
+	  if(code == LV_EVENT_CLICKED){
+		lv_obj_add_flag(btn_ice,LV_OBJ_FLAG_HIDDEN);
+		lv_obj_add_flag(btn_def,LV_OBJ_FLAG_HIDDEN);
+		lv_obj_add_flag(btn_exit,LV_OBJ_FLAG_HIDDEN);
+		game_btn_start_load();
+    game_btn_choose_load();
+	  lv_obj_del(btn_def);
+    lv_obj_del(btn_ice);	  
+    lv_obj_del(btn_exit);
+		game_score = 0;
+		}
+		
+	 
 
-        // 3. 更新最高分
-        if(game_score > high_score) {
-            char buff[20];
-            high_score = game_score;
-            sprintf(buff, "High: %d", high_score);
-            lv_label_set_text(label_high, buff);
-        }
-        
-        // 4. 显示结束界面
-        game_end_show();
-    }
 }
--- game_score.c
+++ game_score.c
@@ -1,6 +1,5 @@
 #include "game.h"
 
-
 uint16_t game_score = 0;
 uint16_t high_score = 0;
 uint16_t game_time  = 60;
@@ -14,13 +13,9 @@ lv_obj_t *game_end_bg;
 lv_obj_t *game_end_title;
 lv_obj_t *game_end_score;
 lv_obj_t *btn_back_main;
-extern lv_timer_t * gametime;
-extern lv_obj_t * btn_exit;
 
 void btn_back_main_cb(lv_event_t *e);
 void game_timer_cb(lv_timer_t* timer);
-
-
 
 void game_score_label_create(lv_obj_t *scr)
 {
@@ -31,7 +26,7 @@ void game_score_label_create(lv_obj_t *scr)
     lv_obj_align(label_score, LV_ALIGN_TOP_LEFT, 20, 100);
 
     label_time = lv_label_create(scr);
-    lv_label_set_text(label_time, "Time: 30");
+    lv_label_set_text(label_time, "Time: 60");
     lv_obj_set_style_text_color(label_time, lv_color_hex(0x00FF00), 0);
 	  lv_obj_set_style_text_font(label_time, &lv_font_montserrat_40, 0);
     lv_obj_align(label_time, LV_ALIGN_TOP_LEFT, 20, 200);
@@ -56,21 +51,20 @@ void game_timer_cb(lv_timer_t* timer)
     {
         game_time--;
 
-        char buff[20];
-        sprintf(buff, "Time: %d", game_time);
-        lv_label_set_text(label_time, buff);
+        char buf[20];
+        sprintf(buf, "Time: %d", game_time);
+        lv_label_set_text(label_time, buf);
 
         if(game_time <= 0)
         {
             game_over = 1;
-					  gametime=NULL;
             lv_timer_del(timer);
 
             if(game_score > high_score)
             {
                 high_score = game_score;
-                sprintf(buff, "High: %d", high_score);
-                lv_label_set_text(label_high, buff);
+                sprintf(buf, "High: %d", high_score);
+                lv_label_set_text(label_high, buf);
             }
 
             game_end_show();
@@ -80,8 +74,6 @@ void game_timer_cb(lv_timer_t* timer)
 
 void game_end_show(void)
 {
-	  char buf[60];
-    lv_anim_del_all();
     game_end_bg = lv_obj_create(lv_scr_act());
     lv_obj_set_size(game_end_bg, LV_HOR_RES, LV_VER_RES);
     lv_obj_set_style_bg_color(game_end_bg, lv_color_black(), 0);
@@ -95,6 +87,7 @@ void game_end_show(void)
     lv_obj_align(game_end_title, LV_ALIGN_CENTER, 0, -50);
 
     game_end_score = lv_label_create(game_end_bg);
+    char buf[60];
     sprintf(buf, "Score: %d\nBest Record: %d", game_score, high_score);
     lv_label_set_text(game_end_score, buf);
     lv_obj_set_style_text_color(game_end_score, lv_color_white(), 0);
@@ -110,58 +103,27 @@ void game_end_show(void)
     lv_obj_center(lab);
 }
 
+// 返回按钮回调（完整保留！）
 void btn_back_main_cb(lv_event_t *e)
 {
-    lv_event_code_t code = lv_event_get_code(e);
-    if(code != LV_EVENT_CLICKED) return;
-    lv_obj_set_parent(btn_exit,scr_menu);
-    // 1. 直接删除结束界面（如果存在）
-    if(game_end_bg != NULL) {
-        lv_obj_del(game_end_bg);
-        game_end_bg = NULL;
-    }
-    
-    // 2. 清理游戏资源
+    lv_obj_del(game_end_bg);
     game_cleanup_all();
-    
-    // 3. 恢复主菜单按钮状态
-    lv_obj_clear_flag(btn_start, LV_OBJ_FLAG_HIDDEN);
-    lv_obj_clear_flag(btn_choose, LV_OBJ_FLAG_HIDDEN);
-    lv_obj_add_flag(btn_def, LV_OBJ_FLAG_HIDDEN);
-    lv_obj_add_flag(btn_ice, LV_OBJ_FLAG_HIDDEN);
-    
-    // 4. 重置Exit按钮
-    lv_obj_set_parent(btn_exit, scr_menu);
-    lv_obj_set_size(btn_exit, 256, 100);
-    lv_obj_align(btn_exit, LV_ALIGN_CENTER, 0, 150);
-    lv_obj_add_flag(btn_exit, LV_OBJ_FLAG_HIDDEN);
-    
-    // 5. 切换回主菜单
     lv_scr_load(scr_menu);
 }
 
+// 清理资源，不清除最高分
 void game_cleanup_all(void)
 {
-    // 1. 清理定时器
-    if(gametime != NULL) {
-        lv_timer_del(gametime);
-        gametime = NULL;
-    }
-    
-    // 2. 清理游戏界面
-    if(scr_game != NULL && lv_obj_is_valid(scr_game)) {
-        lv_obj_clean(scr_game);
-    }
-    
-    // 3. 重置游戏状态
+    lv_timer_pause(NULL);
+    lv_obj_clean(scr_game);
+
     status = NORMAL;
     game_over = 0;
     game_score = 0;
-    game_time = 30;
-    
-    // 4. 重置坐标映射
-    for(uint8_t i = 0; i < GRID_COLS; i++) {
-        for(uint8_t j = 0; j < GRID_ROWS; j++) {
+    game_time = 60;
+
+    for(uint8_t i = 0; i < GRID_COLS; i++){
+        for(uint8_t j = 0; j < GRID_ROWS; j++){
             coord_map[i][j] = NULL;
         }
     }
